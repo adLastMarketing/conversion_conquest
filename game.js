d2e473be3e6bd619ecc81d375f7454b74019ea06
adLastMarketing <148692554+adLastMarketing@users.noreply.github.com>
2023-10-22 08:37:34 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,66 +1,78 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+// ... [PREVIOUS CODE]
 
-let player = {
-    x: canvas.width / 2,
-    y: canvas.height - 50,
-    speed: 5,
-    size: 20,
-};
+let powerUps = [];
+let powerUpChance = 0.1; // 10% chance to spawn a power-up instead of an obstacle
 
-let obstacles = [];
-let score = 0;
+// ... [EXISTING spawnObstacle FUNCTION]
 
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowLeft' && player.x - player.size > 0) {
-        player.x -= player.speed;
-    } else if (event.key === 'ArrowRight' && player.x + player.size < canvas.width) {
-        player.x += player.speed;
-    }
-});
-
-function spawnObstacle() {
-    const size = Math.random() * 30 + 10;
+function spawnPowerUp() {
+    const size = 30;
     const position = Math.random() * (canvas.width - size);
-    obstacles.push({ x: position, y: 0, size: size, speed: 2 });
+    powerUps.push({ x: position, y: 0, size: size, speed: 2, type: Math.random() > 0.5 ? 'rocket' : 'token' });
 }
 
 function updateGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
+    // Draw player
     ctx.fillStyle = 'blue';
     ctx.fillRect(player.x, player.y, player.size, player.size);
 
+    // Draw obstacles
     for (let i = 0; i < obstacles.length; i++) {
-        ctx.fillStyle = 'red';
-        ctx.fillRect(obstacles[i].x, obstacles[i].y, obstacles[i].size, obstacles[i].size);
-        obstacles[i].y += obstacles[i].speed;
+        // ... [PREVIOUS OBSTACLE CODE]
 
-        // Collision detection
-        if (player.x < obstacles[i].x + obstacles[i].size &&
-            player.x + player.size > obstacles[i].x &&
-            player.y < obstacles[i].y + obstacles[i].size &&
-            player.y + player.size > obstacles[i].y) {
-            // Restart the game
-            player.x = canvas.width / 2;
-            player.y = canvas.height - 50;
-            obstacles = [];
-            score = 0;
-            alert('Game Over');
-        }
+        // Detect collision with power-ups
+        for (let j = 0; j < powerUps.length; j++) {
+            if (player.x < powerUps[j].x + powerUps[j].size &&
+                player.x + player.size > powerUps[j].x &&
+                player.y < powerUps[j].y + powerUps[j].size &&
+                player.y + player.size > powerUps[j].y) {
+                
+                if (powerUps[j].type === 'rocket') {
+                    score += 10;
+                } else if (powerUps[j].type === 'token') {
+                    player.size += 5;
+                    setTimeout(() => { player.size -= 5; }, 5000);  // Increase size for 5 seconds
+                }
 
-        // Remove off-screen obstacles
-        if (obstacles[i].y > canvas.height) {
-            obstacles.splice(i, 1);
-            i--;
-            score++;
+                powerUps.splice(j, 1);
+                j--;
+            }
         }
     }
 
-    ctx.fillText(`Score: ${score}`, 10, 10);
+    // Draw power-ups
+    for (let i = 0; i < powerUps.length; i++) {
+        if (powerUps[i].type === 'rocket') {
+            ctx.fillStyle = 'green';
+        } else if (powerUps[i].type === 'token') {
+            ctx.fillStyle = 'gold';
+        }
+        ctx.fillRect(powerUps[i].x, powerUps[i].y, powerUps[i].size, powerUps[i].size);
+        powerUps[i].y += powerUps[i].speed;
+
+        // Remove off-screen power-ups
+        if (powerUps[i].y > canvas.height) {
+            powerUps.splice(i, 1);
+            i--;
+        }
+    }
+
+    // Update score
+    document.getElementById('scoreBoard').innerText = `Score: ${score}`;
 
     requestAnimationFrame(updateGame);
 }
 
-setInterval(spawnObstacle, 2000);
+// Adjusted spawn logic to sometimes spawn power-ups
+function spawnEntity() {
+    if (Math.random() < powerUpChance) {
+        spawnPowerUp();
+    } else {
+        spawnObstacle();
+    }
+}
+
+setInterval(spawnEntity, 2000);
 updateGame();
